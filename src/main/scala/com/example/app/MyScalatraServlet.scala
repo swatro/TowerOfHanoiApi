38167c4bc521ae9db897f47650f7cf7bf79ec50d
Stacey Watro <Staceywatr@gmail.com>
2015-10-26 18:15:51 -0400

    Adds a path parameter for how many disks are in the game. Adds error handling.

--- src/main/scala/com/example/app/MyScalatraServlet.scala
+++ src/main/scala/com/example/app/MyScalatraServlet.scala
@@ -4,10 +4,16 @@ import com.example.app.java.Runner
 
 class MyScalatraServlet extends MyScalatraWebAppStack {
 
-  get("/towerOfHanoi"){
-    new Runner().getResponse(4)
+  get("/towerOfHanoi/:disks"){
+    val disks: String = params("disks")
 
+    if (disks.nonEmpty  && isAllDigits(disks)){
+      new Runner().getResponse(disks.toInt)
+    }
+    else{
+      "Please specify an integer for the number of disks."
+    }
   }
 
-
+  def isAllDigits(x: String) = x forall Character.isDigit
 }
